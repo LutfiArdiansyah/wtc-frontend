442fe3def23abe6366977d3824aa4268c1ae7fa0
Lutfi Ardiansyah <lutfi@kct.co.id>
2021-12-31 21:54:15 +0700

    ministries, landing page adjust

--- application/modules/home/views/index.php
+++ application/modules/home/views/index.php
@@ -32,38 +32,25 @@
 </style>
 <div class="container">
 	<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-		<div class="carousel-indicators">
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-		</div>
 		<div class="carousel-inner">
 			<div class="carousel-item active">
 				<img class="d-block w-100" src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1190,w_2121,x_0,y_176/f_auto,q_auto,w_1100/v1554929522/shape/mentalfloss/538374-istock-940372528.jpg" alt="First slide">
 				<div class="carousel-caption d-none d-md-block">
-					<h5>Register Onsite Service</h5>
-					<p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
+					<div class="row justify-content-center">
+						<div class="col-6 hchy">
+							<button type="button" class="btn btn-primary w-100" style="background-color: #fff; color:#000;">Register Onsite Service</button>
+						</div>
+						<div class="col-6 hchy">
+							<button type="button" class="btn btn-primary w-100" style="background-color: #fff; color:#000;">Watch Online Service</button>
+						</div>
+					</div>
 				</div>
 				<div class="carousel-welcome text-center">
 					<h1>Welcome to</h1>
 					<h1>World Transformation Church</h1>
 				</div>
 			</div>
-			<div class="carousel-item">
-				<img class="d-block w-100" src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1190,w_2121,x_0,y_176/f_auto,q_auto,w_1100/v1554929522/shape/mentalfloss/538374-istock-940372528.jpg" alt="Second slide">
-				<div class="carousel-caption d-none d-md-block">
-					<h5>Watch Online Service</h5>
-					<p>Cras ultricies ligula sed magna dictum porta. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
-				</div>
-			</div>
 		</div>
-		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="visually-hidden">Previous</span>
-		</button>
-		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="visually-hidden">Next</span>
-		</button>
 	</div>
 	<br/>
 	<br/>
--- application/modules/ministries/views/index.php
+++ application/modules/ministries/views/index.php
@@ -1,1 +1,79 @@
-<p>ministriess work!</p>
+<style type="text/css">
+	.carousel-inner, .br-25 { 
+		border-radius: 25px;
+	}
+</style>
+<div class="container">
+	<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		</div>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="https://ychef.files.bbci.co.uk/1600x900/p0b66smr.webp" alt="First slide">
+				<div class="carousel-caption d-none d-md-block">
+					<h5>Ministries</h5>
+					<p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="https://ychef.files.bbci.co.uk/1600x900/p0b66smr.webp" alt="Second slide">
+				<div class="carousel-caption d-none d-md-block">
+					<h5>Ministries</h5>
+					<p>Cras ultricies ligula sed magna dictum porta. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
+				</div>
+			</div>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+	<br/>
+	<br/>
+	<div class="text-center d-flex">
+		<p class="fw-bold w-75 mx-auto">To reach people with the gospel, to disciple them by inculcating the Kingdom of Heaven to become a breakthrough church, to send them as an effective witness in the world.</p>
+	</div>
+	<br/>
+	<br/>
+	<div class="row justify-content-center" >
+		<div class="col-6 text-center">
+			<img src="https://cicikresti.com/wp-content/uploads/2019/11/Serve-from-heart-CRC-Cicik-Resti-768x510.jpg" alt="..." class="w-75 mx-auto br-25">
+		</div>
+		<div class="col-6 text-center ">
+			<img src="https://searchengineland.com/figz/wp-content/seloads/2014/09/outreach-hands-ss-1920-800x450.jpg" alt="..." class="w-75 mx-auto br-25">
+		</div>
+	</div>
+	<br/>
+	<br/>
+	<div class="row justify-content-center" >
+		<div class="col-6 text-center">
+			<h2 class="fw-bold">Serve Within</h2>
+		</div>
+		<div class="col-6 text-center">
+			<h2 class="fw-bold">Outreach</h2>
+		</div>
+	</div>
+	<br/>
+	<div class="row justify-content-center" >
+		<div class="col-6 text-center">
+			<p class="w-50 mx-auto"> ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla paria occaecat cupidatat non proident, sunt in culpa qui officia.</p>
+		</div>
+		<div class="col-6 text-center">
+			<p class="w-50 mx-auto"> ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla paria occaecat cupidatat non proident, sunt in culpa qui officia.</p>
+		</div>
+	</div>
+	<div class="row justify-content-center" >
+		<div class="col-6 text-center">
+			<button type="button" class="btn btn-primary mx-auto w-25">Learn More</button>
+		</div>
+		<div class="col-6 text-center">
+			<button type="button" class="btn btn-primary mx-auto w-25">Learn More</button>
+		</div>
+	</div>
+</div>
