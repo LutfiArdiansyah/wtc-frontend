539bd3d2e761f1acb92c03f6a752cfe1dbc6d866
Lutfi Ardiansyah <lutfi@kct.co.id>
2021-12-31 18:12:13 +0700

    <!-- Grid column --> 			<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4"> 				<!-- Links --> 				<h6 class="text-uppercase fw-bold mb-4"> 					About 				</h6> 				<p> 					<a href="#!" class="text-reset">GBI WTC</a> 				</p> 				<p> 					<a href="#!" class="text-reset">Leadership</a> 				</p> 				<p> 					<a href="#!" class="text-reset">Values</a> 				</p> 			</div> 			<!-- Grid column -->

--- application/modules/templates/views/footer.php
+++ application/modules/templates/views/footer.php
@@ -1,3 +1,119 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted" style="border-radius:25px 25px 0 0 !important;">
+	<!-- Section: Social media -->
+	<section
+	class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+	>
+
+	<!-- Right -->
+	<div>
+		<a href="" class="me-4 text-reset">
+			<i class="fab fa-facebook-f"></i>
+		</a>
+		<a href="" class="me-4 text-reset">
+			<i class="fab fa-twitter"></i>
+		</a>
+		<a href="" class="me-4 text-reset">
+			<i class="fab fa-google"></i>
+		</a>
+		<a href="" class="me-4 text-reset">
+			<i class="fab fa-instagram"></i>
+		</a>
+		<a href="" class="me-4 text-reset">
+			<i class="fab fa-linkedin"></i>
+		</a>
+		<a href="" class="me-4 text-reset">
+			<i class="fab fa-github"></i>
+		</a>
+	</div>
+	<!-- Right -->
+</section>
+<!-- Section: Social media -->
+
+<!-- Section: Links  -->
+<section class="">
+	<div class="container text-center text-md-start mt-5">
+		<!-- Grid row -->
+		<div class="row mt-3">
+			<!-- Grid column -->
+			<div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+				<!-- Content -->
+				<h6 class="text-uppercase fw-bold mb-4">
+					<i class="fas fa-gem me-3"></i>WTC
+				</h6>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				</p>
+			</div>
+			<!-- Grid column -->
+
+			<!-- Grid column -->
+			<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+				<!-- Links -->
+				<h6 class="text-uppercase fw-bold mb-4">
+					About
+				</h6>
+				<p>
+					<a href="#!" class="text-reset">GBI WTC</a>
+				</p>
+				<p>
+					<a href="#!" class="text-reset">Leadership</a>
+				</p>
+				<p>
+					<a href="#!" class="text-reset">Values</a>
+				</p>
+			</div>
+			<!-- Grid column -->
+
+			<!-- Grid column -->
+			<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+				<!-- Links -->
+				<h6 class="text-uppercase fw-bold mb-4">
+					Ministries
+				</h6>
+				<p>
+					<a href="#!" class="text-reset">Serve Within</a>
+				</p>
+				<p>
+					<a href="#!" class="text-reset">Outreach</a>
+				</p>
+			</div>
+			<!-- Grid column -->
+
+			<!-- Grid column -->
+			<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+				<!-- Links -->
+				<h6 class="text-uppercase fw-bold mb-4">
+					Office
+				</h6>
+				<p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+				<p>
+					<i class="fas fa-envelope me-3"></i>
+					info@example.com
+				</p>
+				<p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+				<p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+				<br/>
+				<p><i class="fas fa-print me-3"></i> Tuesday - Saturday</p>
+				<p><i class="fas fa-print me-3"></i> 08:30 AM - 04:00 PM</p>
+				
+			</div>
+			<!-- Grid column -->
+		</div>
+		<!-- Grid row -->
+	</div>
+</section>
+<!-- Section: Links  -->
+
+<!-- Copyright -->
+<div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+	© 2022 - <?php echo date("Y"); ?> Copyright:
+	<a class="text-reset fw-bold" href="<?php echo base_url(); ?>">WTC</a>
+</div>
+<!-- Copyright -->
+</footer>
+<!-- Footer -->
+
 </body>
 <script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/scripts.js"></script>
