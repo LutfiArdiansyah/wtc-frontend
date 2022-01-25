746391847d7c4d333900575223a35fd07eda9cf1
Lutfi Ardiansyah <lutfi@kct.co.id>
2022-01-25 14:56:36 +0700

    font family and grid

--- application/modules/about/views/index.php
+++ application/modules/about/views/index.php
@@ -1,14 +1,15 @@
 <style type="text/css">
 	p.description {
 		white-space: pre-line;
+		font-family: 'Fira Sans', sans-serif;
 	}
 </style>
 <div class="container">
 	<div class="row justify-content-center">
-		<div class="col-lg-4 col-sm-12">
-			<h2 class="fw-bold">About GBI WTC</h2><hr/>
+		<div class="col-lg-3 col-sm-12">
+			<h2 class="fw-bold">About GBI WTC</h2>
 		</div>
-		<div class="col-lg-8 col-sm-12">
+		<div class="col-lg-9 col-sm-12">
 			<div style="background-color:white; border-radius: 25px; padding: 15px;">
 				<p class="description">
 					<?php echo $data->about; ?>
@@ -18,24 +19,24 @@
 	</div>
 	<br/>
 	<div class="row justify-content-center">
-		<div class="col-lg-4 col-sm-12"></div>
-		<div class="col-lg-8 col-sm-12">
+		<div class="col-lg-3 col-sm-12"></div>
+		<div class="col-lg-9 col-sm-12">
 			<div class="row justify-content-center">
-				<div class="col-6">
-					<img class="w-100 h-100" src="<?php echo STRAPI_URL.$data->logo_2->url; ?>">
+				<div class="col-6 d-flex flex-wrap align-items-center">
+					<img class="w-100 " src="<?php echo STRAPI_URL.$data->logo_2->url; ?>">
 				</div>
-				<div class="col-6">
-					<img class="w-100 h-100" src="<?php echo STRAPI_URL.$data->logo_1->url; ?>">
+				<div class="col-6 d-flex flex-wrap align-items-center">
+					<img class="w-100" src="<?php echo STRAPI_URL.$data->logo_1->url; ?>">
 				</div>
 			</div>
 		</div>
 	</div>
 	<br/>
 	<div class="row justify-content-center">
-		<div class="col-lg-4 col-sm-12">
-			<h2 class="fw-bold">Vision</h2><hr/>
+		<div class="col-lg-3 col-sm-12">
+			<h2 class="fw-bold">Vision</h2>
 		</div>
-		<div class="col-lg-8 col-sm-12">
+		<div class="col-lg-9 col-sm-12">
 			<div style="background-color:white; border-radius: 25px; padding: 15px;">
 				<p style="color: var(--bs-primary);"  class="description">
 					<?php echo $data->vision; ?>
@@ -45,10 +46,10 @@
 	</div>
 	<br/>
 	<div class="row justify-content-center">
-		<div class="col-lg-4 col-sm-12">
-			<h2 class="fw-bold">Mision</h2><hr/>
+		<div class="col-lg-3 col-sm-12">
+			<h2 class="fw-bold">Mision</h2>
 		</div>
-		<div class="col-lg-8 col-sm-12">
+		<div class="col-lg-9 col-sm-12">
 			<div style="background-color:white; border-radius: 25px; padding: 15px;">
 				<p style="color: var(--bs-primary);" class="description">
 					<?php echo $data->mission; ?>
@@ -58,18 +59,18 @@
 	</div>
 	<br/>
 	<div class="row justify-content-center">
-		<div class="col-lg-4 col-sm-12">
-			<h2 class="fw-bold">Leadership</h2><hr/>
+		<div class="col-lg-3 col-sm-12">
+			<h2 class="fw-bold">Leadership</h2>
 		</div>
-		<div class="col-lg-8 col-sm-12">
+		<div class="col-lg-9 col-sm-12">
 			<div class="row justify-content-center">
 				<div class="col-12">
 					<?php foreach ($data->wtc_leaderships as $key => $value) {?>
 						<div class="row justify-content-center" style="background-color:white; border-radius: 25px; padding: 15px;">
-							<div class="col-3">
+							<div class="col-4">
 								<img src="<?php echo isset($value->avatar) ? STRAPI_URL.$value->avatar->url : base_url()."assets/img/avatar.svg";?>" alt="..." style="border-radius:50%;width: 100%; height: same-as-width;">
 							</div>
-							<div class="col-9">
+							<div class="col-8">
 								<p class="fw-bold"><?php echo $value->name; ?></p>
 								<p  class="description">
 									<?php echo $value->description; ?>
--- application/modules/templates/views/header.php
+++ application/modules/templates/views/header.php
@@ -10,6 +10,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet">
 	<script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/jquery-3.6.0.min.js"></script>
 </head>
 <body>
