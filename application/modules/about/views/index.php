f2224ec8082f30fd5d42bdabb98504b7de98b900
Lutfi Ardiansyah <lutfi@kct.co.id>
2022-01-30 18:41:29 +0700

    layout about leadership

--- application/modules/about/views/index.php
+++ application/modules/about/views/index.php
@@ -4,10 +4,12 @@
 		font-family: 'Fira Sans', sans-serif;
 		font-size: 1.25rem;
 	}
+
 	.ratio-height {
 		height: 4.094rem;
 		font-size: 1.688rem;
 	}
+
 	.title-about {
 		font-size: 3.438rem;
 	}
@@ -70,38 +72,34 @@
 		<div class="col-lg-3 col-sm-12">
 			<h2 class="fw-bold title-about">Leadership</h2>
 		</div>
-		<div class="col-lg-9 col-sm-12">
-			<div class="row justify-content-center">
-				<div class="col-12">
-					<?php foreach ($data->wtc_leaderships as $key => $value) { ?>
-						<div class="row justify-content-center d-flex align-items-center text-center" style="background-color:white; border-radius: 25px; padding: 15px;">
-							<div class="col-4 text-center">
-								<img src="<?php echo isset($value->avatar) ? STRAPI_URL . $value->avatar->url : base_url() . "assets/img/avatar.svg"; ?>" alt="..." style="border-radius:50%;width: 85%; height: same-as-width;">
-							</div>
-							<div class="col-8">
-								<p class="fw-bold"><?php echo $value->name; ?></p>
-								<p class="description">
-									<?php echo $value->description; ?>
-								</p>
-								<p class="fw-bold">
-									<span><i class="fab fa-instagram"></i>&nbsp;
-										<a href="<?php echo $value->instagram_link_1; ?>" target="_blank"><?php echo $value->instagram_1; ?></a>
+		<div class="col-lg-9 col-sm-12 row">
+			<?php foreach ($data->wtc_leaderships as $key => $value) { ?>
+				<div class="justify-content-center d-flex align-items-center text-center" style="background-color:white; border-radius: 25px; padding: 15px;">
+					<div class="col-4 text-center">
+						<img src="<?php echo isset($value->avatar) ? STRAPI_URL . $value->avatar->url : base_url() . "assets/img/avatar.svg"; ?>" alt="..." style="border-radius:50%;width: 85%; height: same-as-width;">
+					</div>
+					<div class="col-8 text-start">
+						<p class="fw-bold"><?php echo $value->name; ?></p>
+						<p class="description">
+							<?php echo $value->description; ?>
+						</p>
+						<p class="fw-bold">
+							<span><i class="fab fa-instagram"></i>&nbsp;
+								<a href="<?php echo $value->instagram_link_1; ?>" target="_blank"><?php echo $value->instagram_1; ?></a>
 
-									</span>
-									<?php if (isset($value->instagram_2)) { ?>
+							</span>
+							<?php if (isset($value->instagram_2)) { ?>
 
-										&nbsp;
-										<span><i class="fab fa-instagram"></i>&nbsp;
-											<a href="<?php echo $value->instagram_link_2; ?>" target="_blank"><?php echo $value->instagram_2; ?></a>
-										</span>
-									<?php } ?>
-								</p>
-							</div>
-						</div>
-						<br />
-					<?php } ?>
+								&nbsp;
+								<span><i class="fab fa-instagram"></i>&nbsp;
+									<a href="<?php echo $value->instagram_link_2; ?>" target="_blank"><?php echo $value->instagram_2; ?></a>
+								</span>
+							<?php } ?>
+						</p>
+					</div>
 				</div>
-			</div>
+				<br />
+			<?php } ?>
 		</div>
 	</div>
 	<br />
@@ -123,12 +121,12 @@
 		</div>
 		<div class="col-lg-3 col-sm-12">
 			<div>
-				<a href="<?php echo base_url()."spiritualjourney"; ?>">
+				<a href="<?php echo base_url() . "spiritualjourney"; ?>">
 					<button type="button" class="btn btn-primary w-100 br-10 ratio-height" style="background-color: color-primary; color:#fff;">Learn More</button>
 				</a>
 			</div>
 		</div>
 	</div>
 </div>
-<br/>
-<br/>
+<br />
+<br />
