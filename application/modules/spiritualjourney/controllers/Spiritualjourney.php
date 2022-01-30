599d22dfebce6d0dae191d92892ee36d3c0ad139
Lutfi Ardiansyah <lutfi@kct.co.id>
2022-01-30 16:39:31 +0700

    Spiritual Journey

---
+++ application/modules/spiritualjourney/controllers/Spiritualjourney.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Spiritualjourney extends MY_Controller {
+
+	public function index()
+	{
+		$this->load->view('templates/header');
+		$data['datas'] = array("Carecell", "Nextgen", "Ibadah Raya", "Pastoral", "Diklat", "Acara Besar", "Media", "Keuangan");
+		$this->load->view('index', $data);
+		$data['head_office'] = $this->get("/wtc-worship-places/1");
+		$data['minis_cat'] = $this->get("/wtc-ministries-categories");
+		$this->load->view('templates/footer',$data);
+	}
+
+}
+
+/* End of file examples.php */
+/* Location: ./application/modules/examples/controllers/examples.php */
---
+++ application/modules/spiritualjourney/views/index.php
@@ -0,0 +1,94 @@
+<style type="text/css">
+    .title {
+        font-size: 6.25rem;
+    }
+
+    p.description {
+        white-space: pre-line;
+        font-family: 'Fira Sans', sans-serif;
+        font-size: 1.25rem;
+    }
+
+    .parallax {
+        height: 100vh;
+    }
+
+    div.title-parallax {
+        width: 99.1vw;
+        height: 5.375rem;
+        background-color: var(--bs-primary);
+    }
+
+    p.title-parallax {
+        font-size: 1.75rem;
+        color: white;
+    }
+
+    .bg-parallax {
+        background-position-x: center;
+        background-position-y: center;
+        background-attachment: fixed;
+        background-origin: initial;
+        background-clip: initial;
+        background-color: initial;
+        background-size: cover;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+    }
+
+    .content-parallax {
+        color: white;
+        margin-top: 10%;
+    }
+
+    .header-content-parallax {
+        font-size: 6.25rem;
+    }
+</style>
+<h1 class="fw-bold text-center color-primary title">Spiritual Journey</h1>
+<div class="row justify-content-center">
+    <div class="col-lg-8 col-sm-12 text-center">
+        <p class="description text-center">
+            Menolong dan mendorong jemaat secara pribadi aktif berpartisipasi dalam perjalanan pertumbuhan kerohaniannya di dalam gereja dan terlibat dalam gereja sesuai panggilannya. Gereja dan jemaat saling terkoneksi tanpa batas untuk bersama bersinergi dan bertumbuh sebagai satu kesatuan Tubuh Kristus.
+        </p>
+    </div>
+</div>
+<br />
+<br />
+<div class="parallax bg-parallax" style="background-image: url(http://wtc.a-aziz.tech:1337/uploads/7_01d87a7a9b.png);">
+    <div class="title-parallax pt-4">
+        <div class="container">
+            <p class="title-parallax">Spiritual Journey / Lahir Baru -></p>
+        </div>
+    </div>
+    <div class="container content-parallax align-middle">
+        <h1 class="fw-bold header-content-parallax">Lahir Baru</h1>
+        <div class="row mt-5">
+            <div class="col-lg-6 cols-sm-12">
+                <p class="description">To reach people with the gospel, to disciple them by inculcating the Kingdom of Heaven to become a breakthrough church, to send them as an effective witness in the world.</p>
+            </div>
+            <div class="col-lg-6 cols-sm-12">
+                <p class="description">To reach people with the gospel, to disciple them by inculcating the Kingdom of Heaven to become a breakthrough church, to send them as an effective witness in the world.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="parallax bg-parallax" style="background-image: url(http://wtc.a-aziz.tech:1337/uploads/5_34fa626da1.png);">
+    <div class="title-parallax pt-4">
+        <div class="container">
+            <p class="title-parallax">Spiritual Journey / Baptis -></p>
+        </div>
+    </div>
+    <div class="container content-parallax align-middle">
+        <h1 class="fw-bold header-content-parallax">Baptis</h1>
+        <div class="row mt-5">
+            <div class="col-lg-6 cols-sm-12">
+                <p class="description">To reach people with the gospel, to disciple them by inculcating the Kingdom of Heaven to become a breakthrough church, to send them as an effective witness in the world.</p>
+            </div>
+            <div class="col-lg-6 cols-sm-12">
+                <p class="description">To reach people with the gospel, to disciple them by inculcating the Kingdom of Heaven to become a breakthrough church, to send them as an effective witness in the world.</p>
+            </div>
+        </div>
+    </div>
+</div>
