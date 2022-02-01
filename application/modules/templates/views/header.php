0004dd9fd94733dcdf9aaf11e83999bd5bf135b8
Lutfi Ardiansyah <lutfi@kct.co.id>
2022-02-01 16:23:29 +0700

    position internationalization

--- application/modules/templates/views/header.php
+++ application/modules/templates/views/header.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/bootstrap.css?cachebuster=<?php echo date('Ymdhis'); ?>">
 	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/styles.css?cachebuster=<?php echo date('Ymdhis'); ?>">
-	<link rel="icon" href="<?=base_url()?>assets/img/favicon.ico" type="image/gif">
+	<link rel="icon" href="<?= base_url() ?>assets/img/favicon.ico" type="image/gif">
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,13 +16,13 @@
 	<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet">
 	<script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/jquery-3.6.0.min.js"></script>
 	<style type="text/css">
-		.internationalization {
+		/* .internationalization {
 			display: inline-block;
 			position: absolute;
 			top: 81px;
 			right: 0;
 			margin-right: 20px;
-		}
+		} */
 
 		.list-dropdown {
 			max-width: 3.5rem;
@@ -50,8 +50,9 @@
 			top: 0 !important;
 			right: 0 !important;
 			position: absolute !important;
-			margin-right: 1rem;
-			/* margin-top: 1.1rem; */
+			margin-right: 7.5rem;
+			margin-top: 0.7rem;
+			font-weight: bold;
 		}
 	</style>
 </head>
@@ -76,9 +77,16 @@
 		<li class="nav-item">
 			<a class="nav-link" href="<?php echo base_url(); ?>articles?id=1">Global Church</a>
 		</li>
-		<a class="nav-link login-btn" href="<?php echo base_url(); ?>home" style="">
-			<i class="fas fa-sign-in-alt color-primary"></i>&nbsp;Login
-		</a>
+		<div class="dropdown login-btn">
+			<button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+				<i class="fas fa-globe color-primary"></i>&nbsp;<span style="font-weight: bold;">ID</span>
+			</button>
+			<ul class="dropdown-menu list-dropdown" aria-labelledby="dropdownMenuButton1">
+				<li class="border-list-dropdown"><a class="dropdown-item" href="#"><span class="list-language">EN</span></a></li>
+				<li class="border-list-dropdown"><a class="dropdown-item" href="#"><span class="list-language">ID</span></a></li>
+			</ul>
+		</div>
+
 	</ul>
 	<!-- <div class="dropdown internationalization">
 		<button class="btn dropdown-toggle color-primary" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
