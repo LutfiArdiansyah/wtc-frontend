323bad40a1693ccfb4460a4187ff8d28ddcd4f84
Lutfi Ardiansyah <lutfi@kct.co.id>
2022-03-01 12:09:33 +0700

    Update

--- application/modules/give/views/index.php
+++ application/modules/give/views/index.php
@@ -3,67 +3,148 @@
 		display: flex;
 		align-items: center;
 	}
+
 	hr {
-		color: var(--bs-primary);
-		height: 3px;
+		color: #000000;
+		border: 1.5px solid #000000;
+	}
+
+	.container-card {
+		margin-left: 11.125rem;
+		margin-right: 11.125rem;
+	}
+
+	.desc-qris {
+		font-family: 'Fira Sans', sans-serif;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 0.938rem;
+		line-height: 1.25rem;
+	}
+
+	.rekening-title {
+		font-style: normal;
+		font-weight: bold;
+		font-size: 0.938rem;
+		line-height: 1.25rem;
+	}
+
+	.rekening-detail {
+		font-family: 'Fira Sans', sans-serif;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 0.938rem;
+		line-height: 1.25rem;
+		white-space: pre-line;
+	}
+
+	.rekening-detail {
+		margin-bottom: 1.938rem;
 	}
 </style>
 <div class="container">
-	<div class="row justify-content-center">
-		<div class="col-lg-8 col-sm-12">
-			<div class="card br-15">
-				<div class="card-body pt-5">
-					<div class="row">
-						<div class="col-lg-6 col-sm-12">
-							<h1 style="white-space:pre-line" class="fw-bold color-primary text-center">Online <br/> Giving</h1>
-							<br/>
-							<p class="color-primary text-center">Simple and secure</p>
+	<div class="row">
+		<div class="col-lg-12 col-sm-12">
+			<div class="card mx-auto" style="width: 70.25rem; border-radius:1.875rem;">
+				<div class="card-body">
+					<div class="container-card">
+						<div class="row">
+							<div class="col-lg-6 col-sm-12" style="margin-top: 3.625rem;">
+								<h1 style="white-space:pre-line;font-size:4.688rem; font-weight: 600; line-height: 4.394rem;" class="fw-bold color-primary">Online <br /> Giving</h1>
+								<br />
+								<p style="font-size: 1.313rem;">Simple and secure</p>
+							</div>
+							<div class="col-lg-6 col-sm-12 text-center" style="margin-top: 2.719rem;">
+								<a rel='nofollow' href='http://wtc.a-aziz.tech/' border='0' style='cursor:default' target="_blank"><img src='https://chart.googleapis.com/chart?cht=qr&chl=http%3A%2F%2Fwtc.a-aziz.tech%2F&chs=235x235&choe=UTF-8&chld=L|2'></a>
+							</div>
 						</div>
-						<div class="col-lg-6 col-sm-12 text-center">
-							<a rel='nofollow' href='http://wtc.a-aziz.tech/' border='0' style='cursor:default' target="_blank"><img src='https://chart.googleapis.com/chart?cht=qr&chl=http%3A%2F%2Fwtc.a-aziz.tech%2F&chs=180x180&choe=UTF-8&chld=L|2'></a>
+						<br />
+						<br />
+						<div class="row">
+							<div class="col-lg-2 col-sm-12 vertical-center">
+								<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
+									<img class="w-100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Logo_ovo_purple.svg/512px-Logo_ovo_purple.svg.png">
+								</a>
+							</div>
+							<div class="col-lg-2 col-sm-12 vertical-center">
+								<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
+									<img class="w-100" src="https://4.bp.blogspot.com/-1uknxH71s9s/WwPYuFtpahI/AAAAAAAANos/MOR7gGcJR-8hlUQoSk0Tywqo8caAID0hACLcBGAs/s320/Go%2BPay.jpg">
+								</a>
+							</div>
+							<div class="col-lg-2 col-sm-12 vertical-center">
+								<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
+									<img class="w-100" src="https://1.bp.blogspot.com/-GjCpjdW8Hrs/XkXUvE0RseI/AAAAAAAABmk/u5e1zr7RGHQN2TFwPu1IoN8QJBtwXLH5QCLcBGAsYHQ/s400/Logo%2BLink%2BAja%2521.png">
+								</a>
+							</div>
+							<div class="col-lg-2 col-sm-12 vertical-center">
+								<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
+									<img class="w-100" src="https://1.bp.blogspot.com/-0kIXQp9QYBU/XytvNioamkI/AAAAAAAAClo/7p_vwM2w36Yp35CiWjpxF72C4tWJBACpgCLcBGAsYHQ/s400/Logo%2BDana.png">
+								</a>
+							</div>
+							<div class="col-lg-2 col-sm-12 vertical-center">
+								<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
+									<img class="w-100" src="https://1.bp.blogspot.com/-EmJLucvvYZw/X0Gm1J37spI/AAAAAAAAC0s/Dyq4-ko9Eecvg0ostmowa2RToXZITkbcQCLcBGAsYHQ/s400/Logo%2BShopeePay.png">
+								</a>
+							</div>
+							<div class="col-lg-2 col-sm-12 vertical-center">
+								<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
+									<img class="w-100" src="https://1.bp.blogspot.com/-3c9nYA5zsEk/XpMkas_HwlI/AAAAAAAALLQ/8e_GpHCWWfsNR10asOMTxhx-qp-2JYXbwCNcBGAsYHQ/s320/WeChat%2BPay%2B%255Bwww.blogovector.com%255D.png">
+								</a>
+							</div>
 						</div>
-					</div>
-					<br/>
-					<br/>
-					<div class="row">
-						<div class="col-lg-2 col-sm-12 vertical-center">
-							<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
-								<img class="w-100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Logo_ovo_purple.svg/512px-Logo_ovo_purple.svg.png">
-							</a>
+						<hr />
+						<div class="row">
+							<div class="col-lg-4 col-sm-12">
+								<p class="fw-bold mx-auto" style="font-size: 1.547rem;line-height: 1.774rem;">Tentang QRIS</p>
+							</div>
+							<div class="col-lg-8 col-sm-12">
+								<p class="desc-qris">QRIS adalah Quick Response Code Indonesian Standard merupakan standar kode QR Nasional untuk memfasilitasi pembayaran kode QR di Indonesia yang diluncurkan oleh Bank Indonesia dan Asosiasi Sistem Pembayaran Indonesia (ASPI) pada 17 Agustus 2019. Hubungi penyelenggara Bank dan Non-bank anda.<a class="fw-bold color-primary" href=""> disini.</a></p>
+							</div>
 						</div>
-						<div class="col-lg-2 col-sm-12 vertical-center">
-							<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
-								<img class="w-100" src="https://4.bp.blogspot.com/-1uknxH71s9s/WwPYuFtpahI/AAAAAAAANos/MOR7gGcJR-8hlUQoSk0Tywqo8caAID0hACLcBGAs/s320/Go%2BPay.jpg">
-							</a>
-						</div>
-						<div class="col-lg-2 col-sm-12 vertical-center">
-							<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
-								<img class="w-100" src="https://1.bp.blogspot.com/-GjCpjdW8Hrs/XkXUvE0RseI/AAAAAAAABmk/u5e1zr7RGHQN2TFwPu1IoN8QJBtwXLH5QCLcBGAsYHQ/s400/Logo%2BLink%2BAja%2521.png">
-							</a>
-						</div>
-						<div class="col-lg-2 col-sm-12 vertical-center">
-							<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
-								<img class="w-100" src="https://1.bp.blogspot.com/-0kIXQp9QYBU/XytvNioamkI/AAAAAAAAClo/7p_vwM2w36Yp35CiWjpxF72C4tWJBACpgCLcBGAsYHQ/s400/Logo%2BDana.png">
-							</a>
-						</div>
-						<div class="col-lg-2 col-sm-12 vertical-center">
-							<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
-								<img class="w-100" src="https://1.bp.blogspot.com/-EmJLucvvYZw/X0Gm1J37spI/AAAAAAAAC0s/Dyq4-ko9Eecvg0ostmowa2RToXZITkbcQCLcBGAsYHQ/s400/Logo%2BShopeePay.png">
-							</a>
-						</div>
-						<div class="col-lg-2 col-sm-12 vertical-center">
-							<a href="<?php echo base_url() ?>" target="_blank" class="mx-auto">
-								<img class="w-100" src="https://1.bp.blogspot.com/-3c9nYA5zsEk/XpMkas_HwlI/AAAAAAAALLQ/8e_GpHCWWfsNR10asOMTxhx-qp-2JYXbwCNcBGAsYHQ/s320/WeChat%2BPay%2B%255Bwww.blogovector.com%255D.png">
-							</a>
-						</div>
-					</div>
-					<hr/>
-					<div class="row">
-						<div class="col-lg-4 col-sm-12 vertical-center">
-							<p class="color-primary fw-bold mx-auto">What is QRIS?</p>
-						</div>
-						<div class="col-lg-8 col-sm-12">
-							<p>QRIS adalah Quick Response Code Indonesian Standard merupakan standar kode QR Nasional untuk memfasilitasi pembayaran kode QR di Indonesia yang diluncurkan oleh Bank Indonesia dan Asosiasi Sistem Pembayaran Indonesia (ASPI) pada 17 Agustus 2019. Hubungi penyelenggara Bank dan Non-bank anda <a class="fw-bold color-primary" href=""> disini.</a></p>
+						<br />
+						<div class="row">
+							<div class="col-lg-4 col-sm-12">
+								<p style="
+								font-style: normal;
+								font-weight: bold;
+								font-size: 1.547rem;
+								line-height: 1.75rem;
+								">Rekening Persembahan</p>
+								<p style="
+								font-family: 'Fira Sans', sans-serif;
+								font-style: normal;
+								font-size: 0.813rem;
+								line-height: 1rem;
+								">
+									Nama Rekening:
+									<br />
+									<strong>GBI WTC Serpong</strong>
+								</p>
+							</div>
+							<div class="col-lg-8 col-sm-12">
+								<div class="rekening">
+									<p class="rekening-title">Rekening Persembahan/Persepuluhan</p>
+									<p class="rekening-detail">Bank BCA: 545-033-4555
+										Bank CIMB Niaga: 8001-1022-0400</p>
+								</div>
+								<div class="rekening">
+									<p class="rekening-title">Rekening Pembangunan</p>
+									<p class="rekening-detail">Bank BCA: 545-030-3358
+										Bank CIMB Niaga: 8001-1023-3500</p>
+								</div>
+								<div class="rekening">
+									<p class="rekening-title">Rekening Orang Tua Asuh</p>
+									<p class="rekening-detail">Bank BCA: 545-014-7747</p>
+								</div>
+								<div class="rekening">
+									<p class="rekening-title">Rekening Pelayanan Misi</p>
+									<p class="rekening-detail">Bank BCA: 604-073-2177</p>
+								</div>
+								<div class="rekening">
+									<p class="rekening-title">Rekening Pilar Sosial</p>
+									<p class="rekening-detail">Bank BCA: 545-039-2008</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
